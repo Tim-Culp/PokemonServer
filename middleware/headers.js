b226f1a5d5c6552a19b2e3f8f99b0e54ebad6448
Tim <timculpcreations@gmail.com>
2021-04-08 14:41:19 -0400

    testing request process to isolate bug

--- middleware/headers.js
+++ middleware/headers.js
@@ -3,6 +3,8 @@ module.exports = (req, res, next) =>    {
     res.header('access-control-allow-origin', '*');
     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
     res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
+    if(req.method == "OPTIONS"){
+        next();
+    }
     console.log("NEXTED FROM HEADERS")
 }
