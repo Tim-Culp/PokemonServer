aa5125960d4bd4a726761d8201c0d59de33c0e9b
Tim <timculpcreations@gmail.com>
2021-04-08 14:37:08 -0400

    testing request process to isolate bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const Sequelize = require('sequelize');
 const sequelize = require('../db');
 // const userModel = sequelize.import('../models/userModel');
+const Sequelize = require('sequelize');
 const userModel = require('../models/userModel')(sequelize, Sequelize.DataTypes);
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 router.post('/create', (req, res) => {
+    console.log("CREATE BRANCH STARTED")
     userModel.findOne({where:{username:req.body.user.username}})
         .then(response => {
+            console.log("FINDONE COMPLETED")
             if (!response) {
+                console.log("DID NOT FIND USER BY THAT NAME... CREATING USER...")
                 userModel.create({
                     username: req.body.user.username,
                     //register password with bsync encryption
                     passwordhash: bcrypt.hashSync(req.body.user.password, 11)
                 }).then(userData => {
+                    console.log("USER CREATION SUCCESSFUL")
                     //give user session token corresponding to this app's jwt secret
                     //token is created by jwt, a node module which handles the encryption and decryption of web tokens. This token is used by validate-session to determine which user is accessing the server. Validate-session uses jwt to break down the session token into its payload using the jwt secret as the salt, which is then readable.
                     let token = jwt.sign({id: userData.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24})
@@ -26,10 +30,12 @@ router.post('/create', (req, res) => {
                     });
                 },
                 err => {
+                    console.log("USER CREATION UNSUCCESSFUL")
                     res.send(err);
                 })
 
             } else {
+                console.log("USER FOUND BY THAT NAME")
                 res.json({error: "Username already taken.", code:"usernameTaken"})
             }
     
